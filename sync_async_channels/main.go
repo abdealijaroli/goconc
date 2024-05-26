465a8e8b2c89047095628070a8b956bae5887e27
Abdeali Jaroli <abdealiburu03@gmail.com>
2024-05-26 11:10:46 +0530

    sync async channels code

---
+++ sync_async_channels/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan string)
+	go sendData(ch)
+	go getData(ch)
+	time.Sleep(1e9)
+}
+
+func sendData(ch chan string) {
+	ch <- "Washington"
+	ch <- "Tripoli"
+	ch <- "London"
+	ch <- "Beijing"
+	ch <- "Tokyo"
+	close(ch)
+}
+
+func getData(ch chan string) {
+	for val := range ch {
+		fmt.Println(val)
+	}
+}
